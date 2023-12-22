d401b62db0cdb217ab35ddf5b938351106df302c
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2023-12-22 17:04:42 -0500

    Update nf-core_sarek.sh

--- nf-core_sarek.sh
+++ nf-core_sarek.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #SBATCH -J sarek
-#SBATCH --mem=20gb
-#SBATCH --time=48:00:00
+#SBATCH --mem=5gb
+#SBATCH --time=72:00:00
 #SBATCH --output=logs-sarek/%J.log
 #SBATCH --error=logs-sarek/%J.out
-
-module load anaconda3/cpu
-conda activate env_nf
 
 module add singularity/3.9.8
 module add nextflow
